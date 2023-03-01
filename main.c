8075925236791dc8fd2c0e94600b589e95bf6994
sp3p3x <594b4b@gmail.com>
2023-03-02 00:30:16 +0530

    file creation

--- main.c
+++ main.c
@@ -1,10 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void createNote(char filename[255], char content[500]){
+    
+    FILE *file;
+    file = fopen(filename, "w");
+    if(file == NULL)
+    {
+        printf("Error!");   
+        exit(1);             
+    }
+    fprintf(file, "%s", content);
+    fclose(file);
+
+}
 
 int main(){
     char filename[255];
     printf("Enter name of the note: ");
-    scanf("%[^\n]s", filename);
-    printf("%s", filename);
+    fgets(filename, 255, stdin);
+
+    char content[500];
+    printf("Enter the note:\n");
+    fgets(content, 500, stdin);
+    filename[strlen(filename) - 1] = '\0';
+
+    createNote(filename, content);
 
     return 0;
 }
