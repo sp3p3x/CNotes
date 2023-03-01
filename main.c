7d34b1bf7995568e2b138db70366038a52bc73ff
sp3p3x <594b4b@gmail.com>
2023-03-01 23:56:07 +0530

    input filename

--- main.c
+++ main.c
@@ -1,1 +1,10 @@
 #include <stdio.h>
+
+int main(){
+    char filename[255];
+    printf("Enter name of the note: ");
+    scanf("%[^\n]s", filename);
+    printf("%s", filename);
+
+    return 0;
+}
